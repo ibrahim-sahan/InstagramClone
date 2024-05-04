98d2ef856e1ec26986f1c09c34e952034a24d767
ibrahim-sahan <ibrsahan8@gmail.com>
2024-05-04 17:09:31 +0300

    Initial Commit

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by İbrahim Şahan on 4.05.2024.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
