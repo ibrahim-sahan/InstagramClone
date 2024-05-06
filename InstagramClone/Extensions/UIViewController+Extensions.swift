9d578afd6060412fcb74ea1db001e70cdc4af54a
ibrahim-sahan <ibrsahan8@gmail.com>
2024-05-06 08:37:21 +0300

    Add UIViewController extension for presenting alerts

--- InstagramClone/Extensions/UIViewController+Extensions.swift
+++ InstagramClone/Extensions/UIViewController+Extensions.swift
@@ -5,4 +5,15 @@
 //  Created by İbrahim Şahan on 6.05.2024.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    func presentAlert(title: String, message: String, actionTitle: String, actionHandler: (() -> Void)?) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: actionTitle, style: .default) { _ in
+            actionHandler?()
+        }
+        alertController.addAction(action)
+        self.present(alertController, animated: true, completion: nil)
+    }
+}
