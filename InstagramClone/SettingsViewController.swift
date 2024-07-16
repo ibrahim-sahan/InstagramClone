53dc49370d26b5e111d1b4d2767b6ec266265011
ibrahim-sahan <ibrsahan8@gmail.com>
2024-07-17 01:32:19 +0300

    Add sign out functionality

--- InstagramClone/SettingsViewController.swift
+++ InstagramClone/SettingsViewController.swift
@@ -8,13 +8,24 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    
+    private let authService = AuthenticationService()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func logOutClicked(_ sender: Any) {
-        performSegue(withIdentifier: "toViewController", sender: nil)
+        authService.signOut { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success():
+                self.presentAlert(title: "Signed Out", message: "You have been successfully signed out.", actionTitle: "OK") {
+                    self.performSegue(withIdentifier: "toViewController", sender: nil)
+                }
+            case .failure(let error):
+                self.presentAlert(title: "Error", message: "Failed to sign out: \(error.localizedDescription)", actionTitle: "OK", actionHandler: nil)
+            }
+        }
     }
-    
 }
