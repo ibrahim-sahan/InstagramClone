8a9074673b930d22e6fccd87d9f7c2804d0282ea
ibrahim-sahan <ibrsahan8@gmail.com>
2024-07-17 01:28:02 +0300

    Add AuthenticationService class with signIn, signUp, and signOut methods

---
+++ InstagramClone/AuthenticationService.swift
@@ -0,0 +1,41 @@
+//
+//  AuthenticationService.swift
+//  InstagramClone
+//
+//  Created by İbrahim Şahan on 16.07.2024.
+//
+
+import Foundation
+import Firebase
+
+class AuthenticationService {
+    
+    func signIn(email: String, password: String, completion: @escaping (Result<AuthDataResult, Error>) -> Void) {
+        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let authResult = authResult {
+                completion(.success(authResult))
+            }
+        }
+    }
+    
+    func signUp(email: String, password: String, completion: @escaping (Result<AuthDataResult, Error>) -> Void) {
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let authResult = authResult {
+                completion(.success(authResult))
+            }
+        }
+    }
+    
+    func signOut(completion: @escaping (Result<Void, Error>) -> Void) {
+        do {
+            try Auth.auth().signOut()
+            completion(.success(()))
+        } catch let signOutError as NSError {
+            completion(.failure(signOutError))
+        }
+    }
+}
