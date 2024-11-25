389dc03e15255d0e238ba0e74cc3af48b44bdcbb
ibrahim-sahan <ibrsahan8@gmail.com>
2024-11-25 20:53:58 +0300

    Implement basic UITableView

--- InstagramClone/FeedViewController.swift
+++ InstagramClone/FeedViewController.swift
@@ -7,10 +7,24 @@
 
 import UIKit
 
-class FeedViewController: UIViewController {
-
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! FeedCell
+        return cell
+    }
+    
 }
